b29a7852401baaeb1eead7ddf131b757bce9c046
Max Lennon <mrlennon@cs.unc.edu>
2026-03-18 02:22:49 -0400

    Added database insert method

--- db.py
+++ db.py
@@ -1,9 +1,30 @@
 from sqlalchemy import create_engine, MetaData, Table
 
-DATABASE_URL = "postgresql+psycopg://max:postgres@localhost:5432/bball_db"
+from typing import List
+from data_structures import *
+
+DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/bball_db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 metadata = MetaData()
 
 def get_table(name: str) -> Table:
     return Table(name, metadata, autoload_with=engine)
+
+class_table_map = {
+    Comment: "comments",
+    Game: "games",
+    User: "users",
+    Team: "teams",
+    Player: "players",
+    Post: "posts",
+}
+
+# Add a sequence of entries to the database
+# Records can be any mix of Comment, Game, User, etc
+# Table placement is determined by object type
+def insert_pipeline(new_records: List[NamedTuple]):
+    with engine.connect() as conn:
+        for record in new_records:
+            target_table = get_table(class_table_map[type(record)])
+            conn.execute(target_table.insert(), record._asdict())
