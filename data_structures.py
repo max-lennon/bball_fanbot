70f88ef94c1c27c4c675f84dac78a777172fd752
Max Lennon <mrlennon@cs.unc.edu>
2026-03-18 00:51:42 -0400

    Added container classes corresponding to remaining tables in DB

--- data_structures.py
+++ data_structures.py
@@ -8,3 +8,47 @@ class Game(NamedTuple):
     game_thread: str
     post_thread: str
     when_played: str
+
+class Comment(NamedTuple):
+    id: str
+    author: str
+    post_id: str
+    parent_id: str
+    body: str
+    upvotes: int
+    created_at: str
+
+class User(NamedTuple):
+    id: str
+    username: str
+    flair_1: str
+    flair_2: str
+    created_at: str
+
+class Post(NamedTuple):
+    id: str
+    title: str
+    body: str
+    user_id: str
+    category: str
+    upvotes: int
+    created_at: str
+
+class Comment(NamedTuple):
+    id: str
+    author: str
+    post_id: str
+    parent_id: str
+    body: str
+    upvotes: int
+    created_at: str
+
+class Team(NamedTuple):
+    team_name: str
+    conference: str
+    wins: int
+    losses: int
+
+class Player(NamedTuple):
+    player_name: str
+    team_name: str
